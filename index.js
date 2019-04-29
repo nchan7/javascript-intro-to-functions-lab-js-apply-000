8f63adf2911a417f0485388b2d1befd91866b3c0
algebraic-animation-3644 <nathanchan1007@gmail.com>
2019-04-29 05:56:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function logShout(string) {
 function logWhisper(string) {
     console.log(whisper(string))
 }
+function sayHiToGrandma(string) {
+    if(string === lowercase) {
+        return "I can't hear you!"
+    elseif(string === "I love you, Grandma.")
+        return "I love you, too."
+    else
+        return "YES INDEED!"
+    }
+    }
+}
